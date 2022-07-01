b41383eb9b5d1409311e3db70a5479f9fa73691b
Matlaku <108344984+Matlaku@users.noreply.github.com>
2022-07-01 18:52:26 +0200

    Add files via upload

--- SqlQuery_1.sql
+++ SqlQuery_1.sql
@@ -2,19 +2,24 @@
 go
 
 create table POP(
-PopID int not null,
+PopID int identity(1,1) primary key,
 address varchar(255) not null,
 Name varchar(255) not null,
 availability bit
 );
 
 insert into POP
-values (01,'52 Bangay road Montclair','Anelang',1),
-(02,'26 edgewood ave','Nhlakanipho',1),
-(03,'78 gravy lane', 'Zama',0),
-(04,'49 grove lane','Sanele',1);
+values ('52 Bangay road Montclair','Anelang',1),
+('26 edgewood ave','Nhlakanipho',1),
+('78 gravy lane', 'Zama',1),
+('49 grove lane','Sanele',1);
 
 select * from POP;
 
 select PopID from POP where availability = 1;
 
+select Name, address from POP
+
+update POP set availability = 0 where PopID = 1
+
+drop table POP
