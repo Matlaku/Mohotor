54cb5032ecf6ea96565330f9f90c80fa16262552
Matlaku <108344984+Matlaku@users.noreply.github.com>
2022-07-01 18:53:34 +0200

    Add files via upload

--- Mohotor/Program.cs
+++ Mohotor/Program.cs
@@ -36,15 +36,34 @@ namespace Mohotor
 
             if (command == 1)
             {
-                //get info and add to data table
+                //get info about which places are available and add to data table
                 SqlDataAdapter da = new SqlDataAdapter("select PopID from POP where availability = 1", scon);
                 DataTable dt = new DataTable();
                 da.Fill(dt);
 
                 try
                 {
-                    string result = dt.Rows[0][0].ToString();
-                    Console.WriteLine(result);
+                    //PopId is in the result variable
+                    int id = Convert.ToInt32(dt.Rows[0][0]);
+                    Console.WriteLine(id.ToString());
+
+                    //select from the table the first available charging point, add to datatable
+                    SqlDataAdapter selectlocation = new SqlDataAdapter("select Name, address from POP where PopID = " + id + "", scon);
+                    DataTable location = new DataTable();
+                    selectlocation.Fill(location);
+
+                    //get the location and the name from the data table add to 'name' and 'address' variables
+                    string name = location.Rows[0][0].ToString();
+                    string address = location.Rows[0][1].ToString();
+                    Console.WriteLine(name + " has an available charging point at : " + address);
+
+                    //change availability to 0 
+                    //SqlDataAdapter updateavail = new SqlDataAdapter("update POP set availability = 0 where PopID = " + id, scon);
+                    SqlCommand scom = new SqlCommand("update POP set availability = 0 where PopID = " + id, scon);
+                    scon.Open();
+                    scom.ExecuteNonQuery();
+                    scon.Close();
+                    scom.Dispose();
                 }
                 catch
                 {
@@ -53,7 +72,17 @@ namespace Mohotor
             }
             else if(command == 2)
             {
+                Console.WriteLine("Name:");
+                string username = Console.ReadLine();
+                Console.WriteLine("address:");
+                string useraddress = Console.ReadLine();
 
+                //add new user into the database
+                SqlCommand adduser = new SqlCommand("insert into POP values('" + useraddress + "','" + username + "', 1", scon);
+                scon.Open();
+                adduser.ExecuteNonQuery();
+                scon.Close();
+                adduser.Dispose();
             }
             else
             {
